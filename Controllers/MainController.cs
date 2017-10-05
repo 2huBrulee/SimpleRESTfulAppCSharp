dea126e6304d2c06ca202c9193e703b7cefb107b
Cesar Gutierrez Tineo <cesar@tineo.mobi>
2017-10-05 18:35:24 -0500

    main controller

---
+++ Controllers/MainController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleRESTfulApp.Controllers
+{
+    [Route("/")]
+    public class MainController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "lds", "fake" };
+        }
+    }
+}
